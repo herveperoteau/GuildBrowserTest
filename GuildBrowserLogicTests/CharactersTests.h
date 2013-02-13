65e4e8c70fac87a34a25fdb30e8a98a357bda04c
Herve Peroteau <herve.peroteau@gmail.com>
2013-02-13 21:53:13 +0100

    version de test qui bug

---
+++ GuildBrowserLogicTests/CharactersTests.h
@@ -0,0 +1,13 @@
+//
+//  CharactersTests.h
+//  GuildBrowser
+//
+//  Created by Hervé PEROTEAU on 13/02/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharactersTests : SenTestCase
+
+@end
